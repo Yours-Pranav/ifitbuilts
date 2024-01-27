138cde50e2a628908e117769047917e62ddff44f
Pranav <pranavjha0002@gmail.com>
2024-01-27 15:22:41 +0530

    Update and rename sample_config.py to config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -14,9 +14,9 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    API_ID = 24204113  # integer value, dont use ""
+    API_HASH = "d4fad5275d1969ee0e33c92efada3da8"
+    TOKEN = "6844364733:AAF0N22YvmmZOE2YXmeBTtBTFniQLPTn_QY"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
     OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
     OWNER_USERNAME = "Sawada"
     SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
